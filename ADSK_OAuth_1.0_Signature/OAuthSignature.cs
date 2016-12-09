703034515f7ee9193525c30bf8b7559695ea641c
Nakul Sharma <nakulsharma06@gmail.com>
2016-12-09 14:23:26 -0800

    Update OAuthSignature.cs

--- ADSK_OAuth_1.0_Signature/OAuthSignature.cs
+++ ADSK_OAuth_1.0_Signature/OAuthSignature.cs
@@ -12,7 +12,7 @@ namespace ADSK_OAuth_1._0_Signature
             string consumerSecret= "3ff2efbe-a998-4b9f-a791-666c66ece5c7";
             string timestamp = "1477603197";
             string nonce = "xnSGam";
-            string url = "https://accounts-staging.autodesk.com/api/accounts/v1/user";
+            string url = "";
             string s1 = o2.GenerateHeader(url, consumerKey, consumerSecret);
             string s2 = o2.GenerateSignature(url, consumerKey, consumerSecret,null,null, "POST",timestamp,nonce);
             Console.WriteLine(s1);
